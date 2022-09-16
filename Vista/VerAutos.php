e6a54dd59ac3f3c8e3c0734beb01f3074d75d622
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-09-16 17:31:32 -0300

    wi

--- Vista/VerAutos.php
+++ Vista/VerAutos.php
@@ -2,6 +2,7 @@
 include_once 'Common/Header.php';
 include_once 'Common/Footer.php';
 ?>
+
 <div class="container-fluid">
     <div>
         <div class="container col-md-12" style="margin:30px;height: 150vh;">
