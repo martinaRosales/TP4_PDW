e52f9f7d9fdc3cb09ce5114bc698cb468d90114d
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-09-16 18:00:32 -0300

    Cambios

--- Vista/VerAutos.php
+++ Vista/VerAutos.php
@@ -1,11 +1,19 @@
 <?php
 include_once 'Common/Header.php';
 include_once 'Common/Footer.php';
+include_once '../Control/C_Auto.php';
+
+$objControlAuto = new ControlAuto();
+$arrayAutos = $objControlAuto->buscar(NULL);
+$cantidadAutos = count($arrayAutos);
+$i = 0;
 ?>
 
 <div class="container-fluid">
     <div>
         <div class="container col-md-12" style="margin:30px;height: 150vh;">
-        <h1>Ver Autos</h1>
+            <h1>Ver Autos</h1>
+            <?
+            ?>
         </div>
     </div>
