ebea39ac01bf4fcd55937490bfb6a78c5d99c016
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-21 13:24:35 -0300

    Se valida que los campos esten completos

--- Vista/accionBuscarPersona.php
+++ Vista/accionBuscarPersona.php
@@ -13,7 +13,7 @@ $personaDatos=$objControladorPersona->buscar($param);
 
 ?>
 <div class="container-md m-5 mx-auto">
-            <form method="post" action="actualizarDatosPersona.php">
+            <form method="post" action="actualizarDatosPersona.php" class="needs-validation" novalidate>
             <div class="row justify-content-center g-3 px-5">
             <div class="col-6 col-lg-4">
                     <label for="NroDni" class="form-label">DNI</label>
@@ -23,27 +23,57 @@ $personaDatos=$objControladorPersona->buscar($param);
                 <div class="col-6 col-lg-4">
                     <label for="Nombre" class="form-label">Nombre</label>
                     <input type="text" class="form-control" 
-                    value="<?php echo $personaDatos[0]->getNombre()?>" name="Nombre" id="Nombre">
+                    value="<?php echo $personaDatos[0]->getNombre()?>" name="Nombre" id="Nombre" required>
+                    <div class="valid-feedback">
+                    Looks good!
+                    </div>
+                    <div class="invalid-feedback">
+                    Please provide a name.
+                    </div>
                 </div>
                 <div class="col-6 col-lg-4">
                     <label for="Apellido" class="form-label">Apellido</label>
                     <input type="text" class="form-control" 
-                    value="<?php echo $personaDatos[0]->getApellido()?>" name="Apellido" id="Apellido">
+                    value="<?php echo $personaDatos[0]->getApellido()?>" name="Apellido" id="Apellido" required>
+                    <div class="valid-feedback">
+                    Looks good!
+                    </div>
+                    <div class="invalid-feedback">
+                    Please provide a name.
+                    </div>
                 </div>
                 <div class="col-6 col-lg-4">
                     <label for="fechaNac" class="form-label">Fecha de nacimiento</label>
                     <input type="date" class="form-control" 
-                    value="<?php echo $personaDatos[0]->getFechaNac()?>" name="fechaNac" id="fechaNac">
+                    value="<?php echo $personaDatos[0]->getFechaNac()?>" name="fechaNac" id="fechaNac" required>
+                    <div class="valid-feedback">
+                    Looks good!
+                    </div>
+                    <div class="invalid-feedback">
+                    Please provide a date.
+                    </div>
                 </div>
                 <div class="col-6 col-lg-4">
                     <label for="Telefono" class="form-label">Telefono</label>
                     <input type="text" class="form-control" 
-                    value="<?php echo $personaDatos[0]->getTelefono()?>" name="Telefono" id="Telefono">
+                    value="<?php echo $personaDatos[0]->getTelefono()?>" name="Telefono" id="Telefono" required>
+                    <div class="valid-feedback">
+                    Looks good!
+                    </div>
+                    <div class="invalid-feedback">
+                    Please provide a telephone number.
+                    </div>
                 </div>
                 <div class="col-6 col-lg-4">
                     <label for="Domicilio" class="form-label">Domicilio</label>
                     <input type="text" class="form-control" 
-                    value="<?php echo $personaDatos[0]->getDomicilio()?>" name="Domicilio" id="Domicilio">
+                    value="<?php echo $personaDatos[0]->getDomicilio()?>" name="Domicilio" id="Domicilio" required>
+                    <div class="valid-feedback">
+                    Looks good!
+                    </div>
+                    <div class="invalid-feedback">
+                    Please provide an adress.
+                    </div>
                 </div>
                 <div class="text-center">
                     <button type="submit" class="btn btn-primary my-3">MODIFICAR</button>
@@ -52,3 +82,4 @@ $personaDatos=$objControladorPersona->buscar($param);
             
             </form>
 </div>
+<script type="text/javascript" src="../Vista/Assets/Javascript/validarPersona.js"></script>
