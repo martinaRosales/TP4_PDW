4e8450dd6c7d2c3c03ac494c3c6a74f9f3faade2
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-21 09:14:05 -0300

    Se agrega el select para buscar autos de persona según dni

--- Vista/listaPersonas.php
+++ Vista/listaPersonas.php
@@ -7,10 +7,7 @@ $objControlPersona=new C_Persona();
 $arrayPersonas=$objControlPersona->buscar(NULL);
 $cantPersonas=count($arrayPersonas);
 ?>
-<div class="container-md mt-5">
-  <div class=text-center>
-  <a href="seleccionarPersona.php" class="btn btn-primary mb-3">VER AUTOS</a>
-  </div>
+<div class="container-md my-4 mt-5">
 <table class="table">
   <thead>
     <tr>
@@ -45,5 +42,25 @@ $cantPersonas=count($arrayPersonas);
   
 </table>
 </div>
-
+<div class="container-md">
+    <div class="row justify-content-center">
+      <form class="w-50 mt-3" action="accionBuscarAutosdePersona.php" method="post">
+      <h6>Seleccione el dni de la persona a buscar</h6>
+        <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" 
+                name="dni[]" id="dni">
+        <?php 
+            $i=0;
+            while ($i<$cantPersonas){
+                echo "<option value=".$arrayPersonas[$i]->getNro_dni().">"
+                        .$arrayPersonas[$i]->getNro_dni()."</option>";
+                $i++;
+            }
+        ?>
+        </select>
+        <div class="text-center">
+        <button type="submit" class="btn btn-primary my-3">VER AUTOS</button>
+        </div>
+      </form >
+    </div>
+</div>
 <script type="text/javascript" src="Assets/Javascript/verPersonas.js"></script>
