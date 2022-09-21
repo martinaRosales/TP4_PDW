339c00dc3ad5b799b0196ccc56cdc67a1febc59b
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-21 10:56:08 -0300

    Se crea el buscador de personas según dni

--- Vista/buscarPersona.php
+++ Vista/buscarPersona.php
@@ -7,3 +7,25 @@ $objControlPersona=new C_Persona();
 $arrayPersonas=$objControlPersona->buscar(NULL);
 $cantPersonas=count($arrayPersonas);
 ?>
+
+<div class="container-md my-4">
+    <div class="row justify-content-center">
+      <form class="w-50 mt-3" action="accionBuscarPersona.php" method="post">
+      <h6>Seleccione el dni de la persona a buscar</h6>
+        <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" 
+                name="dni[]" id="dni">
+        <?php 
+            $i=0;
+            while ($i<$cantPersonas){
+                echo "<option value=".$arrayPersonas[$i]->getNro_dni().">"
+                        .$arrayPersonas[$i]->getNro_dni()."</option>";
+                $i++;
+            }
+        ?>
+        </select>
+        <div class="text-center">
+        <button type="submit" class="btn btn-primary my-3">VER PERSONA</button>
+        </div>
+      </form >
+    </div>
+</div>
