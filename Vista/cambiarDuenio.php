416b593555e4fddc72e4402657d8caf3ba5d6642
darianasm <darianagsm@gmail.com>
2022-09-22 17:11:03 -0300

    pagina cambiar duenio.php

--- Vista/cambiarDuenio.php
+++ Vista/cambiarDuenio.php
@@ -10,13 +10,13 @@ include_once('../Modelo/Persona.php');
 <div  class="container-fluid">
     
     <!-- container formulario -->
-    <div class="container col-md-5" style="margin:30px;height:200px;">
+    <div class="container col-md-3" style="margin:30px;height:300px;">
         <h2>Cambiar Dueño</h2>
 
         <form class="needs-validation" novalidate id="form_patente" name="form_patente" method="post" action="accionBuscarAuto.php">
     
-        <!-- cont input -->
-        <div class="mb-3">
+        <!-- cont input patente-->
+        <div class="mb-4">
         <label for="" class="form-label">Ingrese número de patente del auto: </label>
         <input type="text" class="form-control" id="patente" name="patente" min="0" required>
 
@@ -32,8 +32,28 @@ include_once('../Modelo/Persona.php');
             </div>
             <!-- valid feedback -->
 
+        </div> 
+        <!-- cont input patente-->   
+
+        <!-- cont input doc-->
+        <div class="mb-3">
+        <label for="" class="form-label">Ingrese dni del dueño a quitar: </label>
+        <input type="text" class="form-control" id="NroDni" name="NroDni" min="0" required>
+
+            <!-- invalid feedback -->
+            <div class="invalid-feedback">
+                Ingrese número de documento válido!
+            </div>
+            <!-- invalid feedback -->
+
+            <!-- valid feedback -->
+            <div class="valid-feedback">
+                 Número de documento válido!
+            </div>
+            <!-- valid feedback -->
+
         </div>    
-        <!-- cont input -->
+        <!-- cont input doc-->
 
         <!-- submit -->
         <div class="mb-3">
@@ -45,7 +65,7 @@ include_once('../Modelo/Persona.php');
 
         <script src="Assets/Javascript/buscarAuto.js"></script>
 
- </div>
+    </div>
     <!-- container formulario -->
 
 </div>
