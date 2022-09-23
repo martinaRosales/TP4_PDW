20ab4d8727ddcb960fc4ef444b2128e9dd17b139
MartinaRosales <martina.rosales@est.fi.uncoma.edu.ar>
2022-09-23 12:08:28 -0300

    Mejoré el logo, ya no parece una dona :)

--- Vista/Common/Header.php
+++ Vista/Common/Header.php
@@ -26,7 +26,7 @@
     <div>
         <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container-fluid">
-            <a class="navbar-brand" href="../Vista/Index.php"><img style="width: 50px;height: 50px;" src="Assets/Img/logoSitio.svg" alt=""></a>
+            <a class="navbar-brand" href="../Vista/Index.php"><img style="width: 50px;height: 50px;" src="Assets/Img/logoSitio.png" alt=""></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
