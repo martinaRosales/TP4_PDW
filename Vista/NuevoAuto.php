d9108d762c701fe9f9e274e41a006823d1763221
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-23 17:13:09 -0300

    Actualiza NuevoAuto.php

--- Vista/NuevoAuto.php
+++ Vista/NuevoAuto.php
@@ -2,22 +2,35 @@
 include_once('Common/Header.php');
 ?>
 
-<div class="container-fluid">
-
-    <h1>Agregar Auto</h1>
+<div class="container-md justify-content-center">
+    <div class="text-center mt-3">
+        <h1>Agregar Auto</h1>
+    </div>
 
     <form action="accionNuevoAuto.php" class="needs-validation m-3" novalidate id="form-nuevoAuto" name="form-nuevoAuto" method="post">
         <div class="row gap-2 justify-content-center">
-            <div class="form-floating col-3">
-                <input type="text" class="form-control" id="marca" name="Marca" placeholder="Marca">
+            <div class="form-floating col-6 col-lg-4">
+                <input type="text" class="form-control" id="Marca" name="Marca" placeholder="Marca" required>
                 <label for="floatingInput">Marca</label>
             </div>
-            <div class="form-floating col-3">
-                <input type="text" class="form-control" id="modelo" name="Modelo" placeholder="Modelo">
+                <div class="invalid-feedback">
+                    Debe ingresar una Marca.
+                </div>
+                <div class="valid-feedback">
+                    Todo correcto!
+                </div>
+            <div class="form-floating col-6 col-lg-4">
+                <input type="text" class="form-control" id="Modelo" name="Modelo" placeholder="Modelo" required>
                 <label for="floatingInput">Modelo</label>
             </div>
-            <div class="form-floating col-3">
-                <input type="text" class="form-control" id="patente" name="Patente" placeholder="Patente" required>
+            <div class="invalid-feedback">
+                    Debe ingresar un Modelo.
+                </div>
+                <div class="valid-feedback">
+                    Todo correcto!
+                </div>
+            <div class="form-floating col-6 col-lg-4">
+                <input type="text" class="form-control" id="Patente" name="Patente" placeholder="Patente" required>
                 <label for="floatingInput">Patente</label>
                 <div class="invalid-feedback">
                     Debe ingresar una patente.
@@ -25,26 +38,28 @@ include_once('Common/Header.php');
                 <div class="valid-feedback">
                     Todo correcto!
                 </div>
-                <div class="form-floating col-3">
-                <input type="number" class="form-control" id="dniDuenio" name="dniDuenio" placeholder="Numero Documento del dueño" required>
+            </div>
+                <div class="form-floating col-6 col-lg-4">
+                <input type="number" class="form-control" id="DniDuenio" name="DniDuenio" placeholder="Numero Documento del dueño" required>
                 <label for="floatingInput">Documento del dueño</label>
                 <div class="invalid-feedback">
-                    Debe ingresar un documento.
+                    Debe ingresar un documento valido.
                 </div>
                 <div class="valid-feedback">
                     Todo correcto!
                 </div>
                 </div>
-        <div>
-
-            <button class="btn btn-secondary" type="reset">Borrar</button>
-            <button class="btn btn-primary" type="Submit">Enviar</button>
+            <div>
+    <div class="text-center mt-3">
+        <button class="btn btn-secondary" type="reset">Borrar</button>
+        <button class="btn btn-primary" type="Submit">Enviar</button>
+    </div>
         </div>
     </form>
 
 
 </div>
-
+<script src="Assets/Javascript/validarRequeridos.js"></script>
 <?php
 include_once('Common/Footer.php');
 ?>
