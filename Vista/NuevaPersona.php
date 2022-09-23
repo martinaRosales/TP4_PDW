17018dd3364f1518b04081c0b24eda49af47b470
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-23 15:57:34 -0300

    Modifico src en nuevaPersona

--- Vista/NuevaPersona.php
+++ Vista/NuevaPersona.php
@@ -53,7 +53,7 @@ include_once('Common/Header.php');
 
 
 </div>
-<script src="Assets/Javascript/validarPersona.js"></script>
+<script src="Assets/Javascript/validarRequeridos.js"></script>
 <?php
 include_once('Common/Footer.php');
 ?>
